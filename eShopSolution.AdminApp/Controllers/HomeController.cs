c25b061b5956ceb698a412a03ece5e9d57b007e3
Nguyen Thanh Tung <TungNTSE171746@fpt.edu.vn>
2024-06-28 16:01:18 +0700

    fixaaaa

--- eShopSolution.AdminApp/Controllers/HomeController.cs
+++ eShopSolution.AdminApp/Controllers/HomeController.cs
@@ -28,7 +28,6 @@ namespace eShopSolution.AdminApp.Controllers
 
         public IActionResult Index()
         {
-            var user = User.Identity.Name;
             return View();
         }       
 
